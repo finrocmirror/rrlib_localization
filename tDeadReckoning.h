309286853353d9fa47b85df6a1f3d48f0997f8e3
Michael Arndt <m_arndt@cs.uni-kl.de>
2014-11-15 12:55:57 +0100

    Make documentation look better in doxygen

--- tDeadReckoning.h
+++ tDeadReckoning.h
@@ -27,10 +27,6 @@
  *
  * \brief   Contains tDeadReckoning
  *
- * \b tDeadReckoning
- *
- * Class containing functions to perform dead reckoning.
- *
  */
 //----------------------------------------------------------------------
 #ifndef __rrlib__localization__tDeadReckoning_h__
@@ -62,8 +58,8 @@ namespace localization
 //----------------------------------------------------------------------
 //! Class containing functions to perform dead reckoning.
 /*!
- * Class containing functions to perform dead reckoning based on trapezoidal approximation of the integration.
- * This class can either be instantiated or the static methods can be used, depending on what is more useful.
+ * The calculations are based on trapezoidal approximation of the integration.
+ * The class can either be instantiated or the static methods can be used, depending on what is more useful.
  */
 class tDeadReckoning
 {
@@ -73,26 +69,26 @@ class tDeadReckoning
 //----------------------------------------------------------------------
 public:
 
-  /** the pose type used in this class */
+  /** The pose type used in this class */
   typedef tUncertainPose3D<> tPose;
-  /** the twist (linear and angular velocities) type used in this class */
+  /** The twist (linear and angular velocities) type used in this class */
   typedef tUncertainTwist3D<> tTwist;
 
   /**
    * Create a new dead reckoning instance with the specified initial pose
-   * @param initial_pose the initial pose to initialize the object with
+   * @param initial_pose The initial pose to initialize the object with
    */
   tDeadReckoning(const tPose &initial_pose = tPose());
 
   /**
    * Set the internal pose to a different one
-   * @param pose the pose to set internal state to
+   * @param pose The pose to set internal state to
    */
   void SetPose(const tPose &pose);
 
   /**
    * Get the internal pose
-   * @return the pose
+   * @return The pose
    */
   const tPose & GetPose() const;
 
@@ -103,8 +99,8 @@ public:
 
   /**
    * Update the internal pose using the twist as well as the elapsed time.
-   * @param twist the linear and angular velocities
-   * @param elapsed_time the elapsed time
+   * @param twist The linear and angular velocities
+   * @param elapsed_time The elapsed time
    */
   void UpdatePose(const tTwist &twist, const rrlib::time::tDuration &elapsed_time);
 
@@ -114,9 +110,9 @@ public:
    * As the previous twist is not known in this method, the integration can only be approximated using the midpoint rule (also called rectangle rule).
    * For more precise results, use the method that also takes the previous twist into account.
    *
-   * @param pose the pose to be updated
-   * @param twist the linear and angular velocities
-   * @param elapsed_time the elapsed time
+   * @param pose The pose to be updated
+   * @param twist The linear and angular velocities
+   * @param elapsed_time The elapsed time
    */
   static void UpdatePose(tPose &pose, const tTwist &twist, const rrlib::time::tDuration &elapsed_time);
 
@@ -125,10 +121,10 @@ public:
    * This is a static member and thus needs no object to operate on.
    * This method takes the previous twist into account to be able to approximate the integral using the trapezoidal rule.
    *
-   * @param pose the pose to be updated
-   * @param previous_twist the linear and angular velocities of the previous time-step
-   * @param twist the currentlinear and angular velocities
-   * @param elapsed_time the elapsed time
+   * @param pose The pose to be updated
+   * @param previous_twist The linear and angular velocities of the previous time-step
+   * @param twist The currentlinear and angular velocities
+   * @param elapsed_time The elapsed time
    */
   static void UpdatePose(tPose &pose, const tTwist &previous_twist, const tTwist &twist, const rrlib::time::tDuration &elapsed_time);
 
@@ -140,7 +136,7 @@ private:
   tPose pose;
   tTwist previous_twist;
 
-  /** indicates whether the previous twist is available, i.e. if we have been updated at least once */
+  /** Indicates whether the previous twist is available, i.e. if we have been updated at least once */
   bool previous_twist_available;
 
 };
